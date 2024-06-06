6752de02723ecbea2460c9d14af6bc844a5ff5c2
becseiszabolcs <becsei.szabi@gmail.com>
2024-06-06 22:52:36 +0200

    header and first pages

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure-qxkbw!c!ndefj^(@go5d5sul+*z7oghc3vzf_(rbw7=kdc1+u%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=['localhost','127.0.0.1']
+#CSRF_TRUSTED_ORIGINS=['<your application URL here>']
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,9 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'frontend')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
